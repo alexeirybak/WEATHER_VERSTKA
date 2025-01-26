3965b3bdd55c0895f7e2c842bf243d06e88b571c
Алексей Рыбак <rybak2108@yandex.ru>
2025-01-26 22:01:35 +0300

    The end

--- src/api/geoData.js
+++ src/api/geoData.js
@@ -1,17 +1,26 @@
 import { apiKey, baseUrl } from "./apiKeyAndHost.js";
-
-const searchForm = document.querySelector(".search-form");
-const cityInput = document.querySelector(".city-input");
-
-searchForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  getGeoData();
-});
+import { cityInput } from "../components/inputForm.js";
+import { showError } from "../components/error.js";
+import { isCyrillic } from "../helpers/checkCyrillic.js";
+import { replaceAbbreviation } from "../helpers/cityAbbreviation.js";
+import { saveCityToLocalStorage } from "../helpers/saveCityToLocalStorage.js";
+import { getWeather, getForecast } from "./getWeatherAndForecast.js";
+import { renderCurrentWeather } from "../components/currentWeather.js";
+import { renderHourlyForecast } from "../components/hourlyForecast.js";
+import { renderDailyForecast } from "../components/dailyForecast.js";
 
 export const getGeoData = async () => {
-  const city = cityInput.value.trim();
+  let city = cityInput.value.trim();
 
-  if (!city) return;
+  if (!city) {
+    return;
+  }
+  if (!isCyrillic(city)) {
+    showError("Проверьте название города");
+    return;
+  }
+
+  city = replaceAbbreviation(city);
 
   try {
     const geoUrl = `${baseUrl}/geo/1.0/direct`;
@@ -31,9 +40,16 @@ export const getGeoData = async () => {
 
     const { lat, lon } = geoData[0];
 
-    console.log(lat, lon);
+    saveCityToLocalStorage(city);
 
+    const weatherData = await getWeather(lat, lon);
+    const forecastData = await getForecast(lat, lon);
+
+    renderCurrentWeather(weatherData, city);
+    renderHourlyForecast(forecastData);
+    renderDailyForecast(forecastData);
   } catch (error) {
     console.error(error.message);
+    showError("Данные не получены");
   }
 };
