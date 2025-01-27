f96a7309b2ee8595f559e0c9fbc0c8e6df9934f8
Алексей Рыбак <rybak2108@yandex.ru>
2025-01-27 22:14:15 +0300

    It's all

--- init.js
+++ init.js
@@ -4,6 +4,7 @@ import { getWeatherByForm } from "./src/components/inputForm.js";
 import { renderCurrentTime } from "./src/helpers/currentTime.js";
 import { geoLocation } from "./src/components/geoLocation.js";
 import { scrollToTop } from "./src/components/scrollToTop.js";
+import { getCurrentYear } from "./src/components/currentYear.js";
 
 export function initApp() {
   switchTheme();
@@ -12,4 +13,5 @@ export function initApp() {
   renderCurrentTime();
   geoLocation();
   scrollToTop();
+  getCurrentYear();
 }
